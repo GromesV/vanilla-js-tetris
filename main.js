4ff441a3c33e354590676ae41eedb6b63f735efc
GromesV <gromes87@gmail.com>
2023-01-01 22:13:21 +0100

    added new functionalities

--- main.js
+++ main.js
@@ -1,32 +1,93 @@
 
+// let TABLE_CANVAS_DEFAULT_COLOR = '#d7d7fc';
+let TABLE_CANVAS_DEFAULT_COLOR = '#f1f1ff';
 
+function paintCellHtmlElement(rowNdx, colNdx, cellVal, color){
+    let elm = document.getElementById(`r${rowNdx}c${colNdx}`);
+    elm.style.backgroundColor = (cellVal ?  color : TABLE_CANVAS_DEFAULT_COLOR);
+}
 
 function Shape(contour){
     //each countour has matrix
-
-    
-
     let contours = {
         'L' : {
             'matrix' : [
+                [1,0],
+                [1,0],
+                [1,1],
+            ],
+            'color' : "#9b5fe0",
+        },
+        'J' : {
+            'matrix' : [
+                [0,1],
+                [0,1],
+                [1,1],
+            ],
+            'color': "#16a4d8",
+        },
+        'T' : {
+            'matrix' : [
+                [0,1,0],
+                [1,1,1],
+            ],
+            'color':"#60dbe8",
+        },
+        'S' : {
+            'matrix' : [
+                [0,1,1],
+                [1,1,0],
+            ],
+            'color' : "#8bd346",
+        },
+        'Z' : {
+            'matrix' : [
+                [1,1,0],
+                [0,1,1],
+            ],
+            'color' : "#efdf48",
+        },
+        'O' : {
+            'matrix' : [
+                [1,1],
+                [1,1],
+            ],
+            'color' : "#f9a52c",
+        },
+        'I' : {
+            'matrix' : [
                 [0,0,1,0],
                 [0,0,1,0],
-                [0,0,1,1],
-                [0,0,0,0],
-            ]
-        }
+                [0,0,1,0],
+                [0,0,1,0],
+            ],
+            'color' : "#d64e12"
+        },
+        
     }
-
-    this.matrix = function(){
+    let color = null;
+    let curPos = [0,0];
+    this.getCurPos = function(){
+        return curPos;
+    }
+    this.setCurPos = function(newVal){
+        curPos = newVal;
+    }
+    this.getColor = function(){
+        return contours[contour].color;
+    }
+    this.getContour = function(){
+        return contour;
+    }
+    this.getMatrixHeight = function(){
+        /* need this func so that we know above which row is it safe to clear trail*/
+        return contours[contour].matrix.length;
+    }
+    this.getMatrix = function(){
         //we can draw each elem on matrix, easier to reason about
+        return contours[contour].matrix;
     }
-    
-    this.getCenter = function(){
-        //each shape must have center arround which one we rotate
-        //
-    }
-
-    this.draw = function(pos){
+    this.draw = function(table){
         /*
             draw on screen. called after any action and timerInterval
             we are drawing rows from our matrix on "canvas" 
@@ -35,112 +96,217 @@ function Shape(contour){
                 while we have rows in matrix to draw
             
         */
+        let pos = this.getCurPos();
         let matrix = contours[contour].matrix;
         let cnvsRowsToDrawNdx = pos[0];
         let mtrxRowNdxForDrawing = matrix.length-1;
         while (cnvsRowsToDrawNdx > -1 && mtrxRowNdxForDrawing > -1){
             let mtrxRow = matrix[mtrxRowNdxForDrawing];
             mtrxRow.forEach((cellVal, cellNdx)=>{
-                let elm = document.getElementById(`r${cnvsRowsToDrawNdx}c${cellNdx}`);
-                let color =  (cellVal ? 'black' : '#d7d7fc');
-                elm.style.backgroundColor = color;
+                let translatedCellNdx = cellNdx + pos[1];
+                // let elm = document.getElementById(`r${cnvsRowsToDrawNdx}c${translatedCellNdx}`);
+                // let color =  (cellVal ? this.getColor() : TABLE_CANVAS_DEFAULT_COLOR);
+                // elm.style.backgroundColor = color;
+                if (table[cnvsRowsToDrawNdx][translatedCellNdx].getState())
+                    return;
+                paintCellHtmlElement(cnvsRowsToDrawNdx,translatedCellNdx,cellVal,this.getColor())
             });
             cnvsRowsToDrawNdx--;
             mtrxRowNdxForDrawing--;
         }
 
+        //clean row abofe the matrix we are working on
+        //that is where matrix "passed down" and made canvas dirty
+        let matrixHeight = matrix.length;
+        let matrixWidth = matrix[0].length;
+        let rowNdxToClear = pos[0] - matrixHeight;
+        if (rowNdxToClear > -1)
+            for (let colNdx = 0; colNdx < matrixWidth; colNdx++) {
+                if (!table[rowNdxToClear][colNdx].getState()){
+                    const elm = document.getElementById(`r${rowNdxToClear}c${colNdx}`)
+                    elm.style.backgroundColor = TABLE_CANVAS_DEFAULT_COLOR;
+                }
+            }
+
     }
 
-    this.spawnShape = function(){
-        //draw initial shape on center
-        //shape bottom left corner is current position of shape on screen
-        //example: r0c2 means shape is just spawned 
-        //draw only top bottom row
-        let curPos = [0,2];
-        
-    }
-    this.move = function(){
-        //add param where
-        //move shape left/right 
-    }
     this.rotate = function(){
         //rotate clockwise  kbd up
     }
     this.throw = function(){
         //throw shape straight at bottom, kbd down
     }
-
-
+    window._shape = this;
 }
 
+function Cell (state=0,color=null,row,col){
+
+    this.getColor = function(){
+        return color;
+    }
+    this.setColor = function(newColor){
+        paintCellHtmlElement(row,col,this.getState(),newColor);
+        color = newColor;
+        return this;
+    }
+    this.getState = function (){
+        return state;
+    }
+    this.setState = function (newState){
+        state = newState;
+        return this;
+    }
+}
 
 function Tetris(rowNo, colNo){
     // let contours = ['L','T','Z'];
-    let contours = ['L'];
-    let curPos = [0,2];
+    let contours = ['Z','L','O','I','T','Z','J'];
+    //state of table, we mark with 1 if brick shape is bound to the cell
+    let table = [];
+    window._table = table;
+    for (let i = 0; i < rowNo; i++) {
+        let rowArr = [];
+        for (let j = 0; j < colNo; j++) {
+            rowArr.push(new Cell(0,null,i,j));
+        }
+        table.push(rowArr);
+    }
+    //stack of shapes, so we can check if some shape didn't appear randomly in last ...10 
+    let stackOfShapes = [];
+
+    function bottomIsReached(shapeCoords){
+        return (shapeCoords[0]+1)==table.length;
+    }
+
+    function shapeTouched(shape, table){
+        let matrix = shape.getMatrix();
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                const val = matrix[i][j];
+                if (val){
+                    cnvsRowBeneathNdx = shape.getCurPos()[0] - matrix.length + i + 2;
+                    if (cnvsRowBeneathNdx > -1 && table[cnvsRowBeneathNdx][j].getState())
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    function pinShapeToTable(shape, table){
+        let coords = shape.getCurPos();
+        let matrix = shape.getMatrix();
+        let color = shape.getColor();
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) { 
+                const element = matrix[i][j];
+                let translatedTableRow = coords[0] - ((matrix.length-1) - i);
+                let translatedTableCol = coords[1] + j;
+                if (element)
+                   table[translatedTableRow][translatedTableCol].setState(element).setColor(color);
+            }
+        }
+        return table;
+    }
+    
+
     this.drawBoard = function(){
+        if (window.hasBoard){return;}
         let container = document.getElementById('tet-cont');
-        let rows = new Array(rowNo).fill(1);
-        let cols = new Array(colNo).fill(1);
-        rows.forEach( (_, rowNdx)=>{
-            const rowDiv = document.createElement('div');
+        const HTML_ELEMENT = 'div';
+        table.forEach( (row, rowNdx)=>{
+            const rowDiv = document.createElement(HTML_ELEMENT);
             rowDiv.setAttribute('id', `r${rowNdx}`);
             rowDiv.setAttribute('class', 'row');
             container.appendChild(rowDiv);
-            cols.forEach(( _ , colNdx) =>{
-                const cellDiv = document.createElement('div');
+            row.forEach(( _ , colNdx) =>{
+                const cellDiv = document.createElement(HTML_ELEMENT);
                 cellDiv.setAttribute('id', `r${rowNdx}c${colNdx}`);
                 cellDiv.setAttribute('class', 'cell');
+                cellDiv.style.backgroundColor = TABLE_CANVAS_DEFAULT_COLOR;
                 rowDiv.appendChild(cellDiv);
             });
         });
+        window.hasBoard = true;
+
     }
+
+    function* shapeGenerator(){
+        yield 'L'
+        yield 'Z'
+    }
+    let g = shapeGenerator();
+
     this.getRandomShape = function(){
-        const randomElement = contours[Math.floor(Math.random() * contours.length)];
-        return new Shape(randomElement);
-    }
-
-    this.clearTrail = function(){
-        //clean row abofe the matrix we are working on
-        //that is where matrix "passed down" and made canvas dirty
-        let rowNdxToClear = curPos[0]-4;
-        if (rowNdxToClear > -1)
-            for (let index = 0; index < colNo; index++) {
-                const elm = document.getElementById(`r${rowNdxToClear}c${index}`)
-                elm.style.backgroundColor = '#d7d7fc';
+        // return new Shape(g.next().value);
+        let hasTen = (stackOfShapes.length >= 10);
+        if (hasTen){
+            for (let contour of contours){
+                if (stackOfShapes.indexOf(contour) < 0){
+                    stackOfShapes.shift();
+                    stackOfShapes.push(contour);
+                    console.log(`pseudo random: ${contour}`);
+                    return new Shape(contour);
+                }
             }
+        }
+        const rndContour = contours[Math.floor(Math.random() * contours.length)];
+        stackOfShapes.push(rndContour);
+        return new Shape(rndContour);
     }
-
     this.tick = function(){
-        let sh = this.getRandomShape();
-        let ticker = 0;
-        setInterval((x)=>{
-            sh.draw(curPos);
-            this.clearTrail();
-            curPos[0]++;
-            ticker++;
-        }, 1100, curPos, ticker);
-
-        // let tick = 0;
-        // setInterval(
-        //     function test(){
-        //         let el = document.getElementById('r1c1');
-        //         tick % 2 == 0 ? el.style.backgroundColor = 'red' : el.style.backgroundColor = 'green';
-        //         tick++;
-        //     },
-        //     1000
-        // );
+        let shape = this.getRandomShape();
+        window._shape = shape;
+        const shapeReleaser = () => {
+            shape.draw(table);
+            // this.clearTrail(shape.getMatrixHeight());
+            if (bottomIsReached(shape.getCurPos()) || shapeTouched(shape, table)){
+                 //we give player 0.5 sec so he can rotate in place
+                 table = pinShapeToTable(shape, table);
+                 shape = this.getRandomShape();
+                 setTimeout(shapeReleaser, 500);
+            }
+            else
+                shape.setCurPos([shape.getCurPos()[0]+1, shape.getCurPos()[1]]);
+        }
+        
+        let intervalId = setInterval(shapeReleaser, 200);
+        return intervalId;
     }
+
+
 
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    let intervalId;
     let btnStart = document.getElementById('start');
+    let btnStop = document.getElementById('stop');
     btnStart.onmouseup = () =>{
-        const T = new Tetris(14,8);
+        const T = new Tetris(20,10);
         T.drawBoard();
-        T.tick();
+        intervalId = T.tick();
     }
+    btnStop.onmouseup = () =>{
+        clearInterval(intervalId)
+    }
+});
 
+window.addEventListener("keydown", (event) => {
+    if (event.isComposing || event.keyCode === 229) {
+      return;
+    }
+    let shape = window._shape;
+    let table = window._table;
+    switch (event.key) {
+        case 'ArrowRight':
+            shape.setCurPos([shape.getCurPos()[0], shape.getCurPos()[1]+1]);
+            shape.draw(table);
+            break;
+    
+        default:
+            break;
+    }
+    console.log('ada')
 });
 
